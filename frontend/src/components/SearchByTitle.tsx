7a56c22409ab0f5a9aecd4f42ade1e9bd4c4669a
Huzaifa-Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2023-08-25 16:22:27 +0500

    added favorite blog and user-blog pages and enhance the functionality of searching blogs

--- frontend/src/components/SearchByTitle.tsx
+++ frontend/src/components/SearchByTitle.tsx
@@ -1,37 +1,52 @@
+import { useState } from "react";
 import { Search } from "lucide-react";
 import { useQueryClient } from "react-query";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { getBlogsByTitleName } from "@/api/api";
+import { X } from "lucide-react";
 
 export default function SearchByTitle() {
+  const [isSearched, setIsSearched] = useState(false);
   const client = useQueryClient();
 
   const search = async (e: React.FormEvent) => {
     e.preventDefault();
     const titleInput = document.getElementById("title") as HTMLInputElement;
     const title: string = titleInput?.value;
-    const updatedData = await getBlogsByTitleName(title);
-    console.log(updatedData);
-    client.setQueryData("blogs", updatedData);
+    console.log(title);
+    if (title === "") {
+      client.invalidateQueries("blogs");
+      setIsSearched(false);
+    } else {
+      const updatedData = await getBlogsByTitleName(title);
+      client.setQueryData("blogs", updatedData);
+      setIsSearched(true);
+    }
+  };
+
+  const resetSearch = (e: React.FormEvent) => {
+    client.invalidateQueries("blogs");
+    setIsSearched(false);
   };
 
   return (
     <form
       onSubmit={search}
+      onReset={resetSearch}
       className="flex items-center bg-accent w-full text-accent-foreground shadow px-2 rounded text-sm"
     >
       <Input
         type="text"
         placeholder="Enter a title"
         id="title"
-        className="border-none  bg-transparent p-0 focus-visible:ring-transparent focus:border-none m-0"
+        className="border-none bg-transparent p-0 focus-visible:ring-transparent focus-visible:border-none m-0"
       />
       <Button
-        type="submit"
+        type={isSearched ? "reset" : "submit"}
         className="bg-transparent text-accent-foreground p-0"
       >
-        <Search />
+        {isSearched ? <X /> : <Search />}
       </Button>
     </form>
   );
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -5,6 +5,8 @@ import { getAllBlogs } from "@/api/api";
 import BlogCard from "@/components/blogCard/BlogCard";
 import SearchLayout from "@/layouts/SearchLayout";
 import BlogLoading from "@/components/BlogLoading";
+import SearchByCategory from "@/components/SearchByCategory";
+import SearchByTitle from "@/components/SearchByTitle";
 
 export default function Home() {
   const [isLogin] = useUserStore((state) => [state.isLogIn]);
@@ -14,11 +16,15 @@ export default function Home() {
     return <Navigate to="/" />;
   } else {
     return (
-      <SearchLayout>
+      <>
+        <div className="flex flex-col gap-2 sm:flex-row justify-between mb-4">
+          <SearchByTitle />
+          <SearchByCategory />
+        </div>
         <section className="flex flex-col">
           <main className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4">
             {blog.data === null ? (
-              <p>Huzaifa</p>
+              <p className="text-3xl">Sorry!!! Nothing Found Here.</p>
             ) : blog.isLoading ? (
               <BlogLoading />
             ) : (
@@ -28,7 +34,7 @@ export default function Home() {
             )}
           </main>
         </section>
-      </SearchLayout>
+      </>
     );
   }
 }
